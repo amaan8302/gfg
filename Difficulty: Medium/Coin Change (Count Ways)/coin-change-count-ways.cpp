c3dbffe1353ae23ab4de546fbe427dc8a7a88c40
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-11-02 00:11:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
@@ -2,47 +2,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 class Solution {
   public:
-    long long int solve(int i, int amount, int coins[], int n, vector<vector<long long int>>& dp) {
-    if (amount == 0)
-        return 1;
-    if (i >= n || amount < 0)
-        return 0;
-    if (dp[i][amount] != -1)
-        return dp[i][amount];
-
-    long long int take = 0;
-    if (coins[i] <= amount)
-        take = solve(i, amount - coins[i], coins, n, dp);
-    long long int skip = solve(i + 1, amount, coins, n, dp);
-    
-    return dp[i][amount] = take + skip;
-}
-
-long long int count(int coins[], int n, int amount) {
-    vector<vector<long long int>> dp(n, vector<long long int>(amount + 1, -1));
-    return solve(0, amount, coins, n, dp);
-}
-
+    long long int solve(int i, int amount,vector<int>& coins,int n,vector<vector<long long int>>&dp)
+    {
+        if(amount==0)
+            return 1;
+        if(i>=n)
+            return 0;
+        if(coins[i]>amount)
+            return solve(i+1,amount,coins,n,dp);
+        if(dp[i][amount]!=-1)
+            return dp[i][amount];
+        long long int take = solve(i,amount - coins[i] , coins,n,dp);
+        long long int skip = solve(i+1 , amount , coins,n,dp);
+        return dp[i][amount] = take + skip;
+    }
+    long long int count(vector<int> coins, int amount) {
+        // code here.
+        int n = coins.size();
+        vector<vector<long long int>>dp(n+1,vector<long long int>(amount+1,-1));
+        return solve(0,amount,coins,n,dp);
+    }
 };
 
 //{ Driver Code Starts.
+
 int main() {
+
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int sum, N;
-        cin >> sum >> N;
-        int coins[N];
-        for (int i = 0; i < N; i++) cin >> coins[i];
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int sum;
+        cin >> sum;
+        cin.ignore();
         Solution ob;
-        cout << ob.count(coins, N, sum) << endl;
+        cout << ob.count(arr, sum) << endl;
+        cout << "~" << endl;
     }
 
     return 0;
 }
-
 
 // } Driver Code Ends
