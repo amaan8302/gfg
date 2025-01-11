bd87aa870c1f80343a1eded8d5f4e4d6696c97b1
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-01-11 16:51:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -9,27 +9,56 @@ using namespace std;
 
 class Solution {
   public:
-    bool solve(int i, vector<int>& arr, int sum, int n, vector<vector<int>>& dp) {
-    if (sum == 0)
-        return true;
-    if (i >= n || sum < 0)
-        return false;
-    if (dp[i][sum] != -1)
-        return dp[i][sum];
-    
-    bool take = false;
-    if (sum >= arr[i])
-        take = solve(i + 1, arr, sum - arr[i], n, dp);
-
-    bool no = solve(i + 1, arr, sum, n, dp);
-    
-    return dp[i][sum] = take || no;
-}
-
-bool isSubsetSum(vector<int>& arr, int sum) {
+    // bool solve(vector<int>& arr, int target,int idx,vector<vector<int>>&dp)
+    // {
+    //     if(idx==arr.size())
+    //         return 0;
+    //     if(target==0)
+    //         return 1;
+    //     if(dp[idx][target]!=-1)
+    //         return dp[idx][target];
+    //     bool incl = solve(arr,target-arr[idx],idx+1,dp);
+    //     bool excl = solve(arr,target,idx+1,dp);
+    //     return incl||excl;
+    // }
+    // bool isSubsetSum(vector<int>& arr, int target) {
+    //     if(arr.size()==0)
+    //     {
+    //         if(target==0)
+    //             return 1;
+    //         else
+    //             return 0;
+    //     }
+    //     if(arr.size()==1)
+    //     {
+    //         if(target==arr[0])
+    //             return true;
+    //         else 
+    //             return false;
+    //     }
+    //     vector<vector<int>>dp(arr.size()+1,vector<int>(target+1,-1));
+    //     return solve(arr,target,0,dp);
+    // }
+    bool isSubsetSum(vector<int>& arr, int target) {
     int n = arr.size();
-    vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
-    return solve(0, arr, sum, n, dp);
+    vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+
+    // Base case: target 0 is always achievable with an empty subset
+    for (int i = 0; i <= n; ++i) {
+        dp[i][0] = true;
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= target; ++j) {
+            if (arr[i - 1] <= j) {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[n][target];
 }
 
 };
