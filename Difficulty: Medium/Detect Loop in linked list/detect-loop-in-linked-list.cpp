7ae67c7a218258b01c3b9d6e703ceab88c7e9689
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-01-24 12:41:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.cpp
+++ Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.cpp
@@ -24,36 +24,27 @@ void loopHere(Node* head, Node* tail, int position) {
 
 
 // } Driver Code Ends
-// User function template for C++
 
-/*
-
-struct Node
-{
-    int data;
-    struct Node *next;
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-
-*/
 class Solution {
   public:
-    bool detectLoop(Node* head) {
+    // Function to check if the linked list has a loop.
+    bool detectLoop(Node* head) 
+    {
+        if (head == NULL || head->next == NULL)
+            return false;
         Node* slow = head;
         Node* fast = head->next;
-        while(fast!=NULL && slow!=NULL)
-        {
+        while (fast != NULL && fast->next != NULL) {
             fast = fast->next;
-            if(fast!=NULL)
+            if (fast != NULL)
                 fast = fast->next;
             slow = slow->next;
-            if(fast==slow)
-                return 1;
+            if (fast == slow)
+                return true;
         }
-        return 0;
+        return false;
     }
+
 };
 
 
