398cbbeba760a57f5361ef491d374757d5349317
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-02-27 11:58:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
@@ -5,33 +5,25 @@ using namespace std;
 
 // } Driver Code Ends
 
+
 class Solution {
   public:
-    bool bs(vector<int> &arr , int target)
-    {
-        int low = 0,high = arr.size()-1;
-        while(low<=high)
-        {
-            int mid = low + (high-low)/2;
-            if(arr[mid]==target)
-                return 1;
-            else if(arr[mid]>target)
-                high = mid-1;
-            else
-                low = mid+1;
-        }
-        return false;
-    }
     int missingNumber(vector<int> &arr) {
-        std::sort(arr.begin(),arr.end());
-        for(int i = 1 ; i <= 1e5 ; i++)
+        int n = arr.size();
+        vector<bool>seen(n+1,0);
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i]>=0 && arr[i]<=n+1)
+                seen[arr[i]]=true;
+        }
+        for(int i = 1 ; i<=n;i++)
         {
-            if(!bs(arr,i))
+            if(seen[i]==false)
                 return i;
         }
-        return 0;
+        return n+1;
     }
 };
+
 
 //{ Driver Code Starts.
 
