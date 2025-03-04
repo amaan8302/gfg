febd4b806d646a748435215251f178006e5655fd
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-03-04 17:59:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -1,53 +1,53 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 
 // } Driver Code Ends
 
-
-class Solution
-{
-    public:
-    int longestSubsequence(int n, int a[])
-    {
-        
+class Solution {
+  public:
+    int lis(vector<int>& arr) {
         std::vector<int> dp;
-    
-        for (int i = 0; i < n; i++) {
-            auto it = std::lower_bound(dp.begin(), dp.end(), a[i]);
-            
-            if (it == dp.end()) {
-                dp.push_back(a[i]);
-            } else {
-                *it = a[i];
-            }
+        int n = arr.size();
+        for (int i = 0; i < n; i++) 
+        {
+            auto it = std::lower_bound(dp.begin(), dp.end(), arr[i]);
+            if (it == dp.end())
+                dp.push_back(arr[i]);
+            else
+                *it = arr[i];
         }
-        
         return dp.size();
     }
 };
 
+
 //{ Driver Code Starts.
-int main()
-{
-    //taking total testcases
-    int t,n;
-    cin>>t;
-    while(t--)
-    {
-        //taking size of array
-        cin>>n;
-        int a[n];
-        
-        //inserting elements to the array
-        for(int i=0;i<n;i++)
-            cin>>a[i];
-        Solution ob;
-        //calling method longestSubsequence()
-        cout << ob.longestSubsequence(n, a) << endl;
-    
-cout << "~" << "\n";
-}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+
+    while (t--) {
+        int n;
+        vector<int> arr;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution obj;
+        cout << obj.lis(arr) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
 }
 
 // } Driver Code Ends
