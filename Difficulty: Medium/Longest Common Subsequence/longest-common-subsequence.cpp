0e5ed050907839caeff5cba07c9117b0d6d96cce
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-03-06 11:21:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -1,15 +1,12 @@
 //{ Driver Code Starts
 #include <bits/stdc++.h>
-const int mod = 1e9 + 7;
 using namespace std;
 
 
 // } Driver Code Ends
-// function to find longest common subsequence
 
 class Solution {
   public:
-    // Function to find the length of longest common subsequence in two strings.
     int solve(string &a, string &b,int i , int j,vector<vector<int>>&dp)
     {
         if(i==a.size()||j==b.size())
@@ -22,24 +19,26 @@ class Solution {
             dp[i][j] = max(solve(a,b,i+1,j,dp),solve(a,b,i,j+1,dp));
         return dp[i][j];
     }
-    int lcs(int n, int m, string str1, string str2) {
-        // your code here
+    int lcs(string &s1, string &s2) {
+        // code here
+        int n = s1.length(),m = s2.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(str1,str2,0,0,dp);
+        return solve(s1,s2,0,0,dp);
     }
 };
 
 
+
 //{ Driver Code Starts.
 int main() {
-    int t, n, m;
+    int t;
     cin >> t;
     while (t--) {
-        cin >> n >> m; // Take size of both the strings as input
-        string str1, str2;
-        cin >> str1 >> str2; // Take both the string as input
+        string s1, s2;
+        cin >> s1 >> s2; // Take both the strings as input
         Solution ob;
-        cout << ob.lcs(n, m, str1, str2) << endl;
+        cout << ob.lcs(s1, s2) << endl; // Call the lcs function
+        cout << "~\n";
     }
     return 0;
 }
