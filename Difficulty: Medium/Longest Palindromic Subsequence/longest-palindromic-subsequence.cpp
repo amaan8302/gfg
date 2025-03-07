0fcda7c983b891365c0be58f6afd9fcdb1b95176
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-03-07 08:46:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -1,14 +1,15 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-//User function Template for C++
 
-class Solution{
+// User function Template for C++
+
+class Solution {
   public:
     int solve(string a , string b, int i , int j , vector<vector<int>>&dp)
     {
@@ -22,29 +23,45 @@ class Solution{
             dp[i][j] = max(solve(a,b,i+1,j,dp),solve(a,b,i,j+1,dp));
         return dp[i][j];
     }
-    int longestPalinSubseq(string a) {
+    int longestPalinSubseq(string &a) {
+        // string b = a;
+        // std::reverse(a.begin(), a.end());
+        // int n = a.length();
+        // vector<vector<int>>dp(n+1, vector<int>(n+1,-1));
+        // return solve(a,b,0,0,dp);
         string b = a;
         std::reverse(a.begin(), a.end());
         int n = a.length();
-        vector<vector<int>>dp(n+1, vector<int>(n+1,-1));
-        return solve(a,b,0,0,dp);
+        vector<vector<int>>dp(n+1, vector<int>(n+1,0));
+        for(int i = n-1 ; i >=0 ; i--)
+        {
+            for(int j = n-1 ; j >= 0 ; j--)
+            {
+                if(a[i]==b[j])
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                else
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
 
+
 //{ Driver Code Starts.
 
-int32_t main()
-{
-    int t; cin >> t;
-    while (t--)
-    {
-        string s; cin >> s;
+int32_t main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
         Solution ob;
         cout << ob.longestPalinSubseq(s) << endl;
-    
-cout << "~" << "\n";
+
+        cout << "~"
+             << "\n";
+    }
 }
-}
-// Contributed By: Pranay Bansal
 
 // } Driver Code Ends
