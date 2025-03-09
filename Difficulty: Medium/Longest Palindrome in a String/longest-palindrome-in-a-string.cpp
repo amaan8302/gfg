e42b5397c8382cec472500920333f56e03c92aee
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-03-09 17:28:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
@@ -7,38 +7,30 @@ using namespace std;
 
 class Solution {
   public:
-    int leng(int left, int right , string &s)
-    {
-        int len = 0;
-        while(left>=0 && right < s.length())
-        {
-            if(s[left]==s[right])
-            {
-                len+=2;left--;right++;
-            }
-            else
-                break;
+    string expandAroundCenter(string s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
         }
-        return len;
+        return s.substr(left + 1, right - left - 1);
     }
     string longestPalindrome(string &s) {
         int n = s.length();
-        int maxLen = 1,start = 0;
-        for(int i =  0 ; i < n ; i++)
-        {
-            int len = 1 + leng(i-1,i+1,s);
-            if(len>maxLen){
-            maxLen = len;
-            start = i - len/2;}
+        if (n == 0) return "";
+        string longest = "";
+        for (int i = 0; i < n; i++) {
+            string oddPal = expandAroundCenter(s, i, i);
+            if (oddPal.length() > longest.length()) {
+                longest = oddPal;
+            }
+
+            string evenPal = expandAroundCenter(s, i, i + 1);
+            if (evenPal.length() > longest.length()) {
+                longest = evenPal;
+            }
         }
-        for(int i =  1 ; i < n ; i++)
-        {
-            int len = leng(i-1,i,s);
-            if(len>maxLen){
-            maxLen = len;
-            start = i - len/2;}
-        }
-        return s.substr(start , maxLen);
+
+        return longest;
     }
 };
 
