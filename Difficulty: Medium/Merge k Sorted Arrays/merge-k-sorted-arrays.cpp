5b90081ffabe3f7040e84e1cdd4d723b0763450a
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-03-26 22:55:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
@@ -1,56 +1,69 @@
 //{ Driver Code Starts
-// Initial Template for C++
+//Initial Template for C++
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define N 105
 using namespace std;
-
-void printArray(vector<int> arr, int size) {
-    for (int i = 0; i < size; i++)
-        cout << arr[i] << " ";
+void printArray(vector<int> arr, int size)
+{
+for (int i=0; i < size; i++)
+	cout << arr[i] << " ";
 }
 
 
 // } Driver Code Ends
-// User function Template for C++
 
-class Solution {
-  public:
-    vector<int> mergeKArrays(vector<vector<int>>& arr, int k) {
-        vector<int>r;
-        for(int i = 0 ; i < k ; i++)
+class Solution
+{
+    public:
+    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
+    {
+        priority_queue<int , vector<int>,greater<int>>pq;
+        for (auto &row : arr) 
         {
-            for(int j = 0 ; j < k ; j++)
-                r.push_back(arr[i][j]);
+            for (int num : row)
+                pq.push(num);
         }
-        sort(r.begin(),r.end());
-        return r;
+        vector<int>ans;
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
     }
 };
 
+
 //{ Driver Code Starts.
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int k;
-        cin >> k;
-        vector<vector<int>> arr(k, vector<int>(k, 0));
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < k; j++) {
-                cin >> arr[i][j];
-            }
-        }
-        Solution obj;
-        vector<int> output = obj.mergeKArrays(arr, k);
-        printArray(output, k * k);
-        cout << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+	    int k;
+	    cin>>k;
+	    vector<vector<int>> arr(k, vector<int> (k, 0));
+	    for(int i=0; i<k; i++){
+	        for(int j=0; j<k; j++)
+	        {
+	            cin>>arr[i][j];
+	        }
+	    }
+	    Solution obj;
+    	vector<int> output = obj.mergeKArrays(arr, k);
+    	printArray(output, k*k);
+    	cout<<endl;
+    
+cout << "~" << "\n";
 }
+	return 0;
+}
+
+
+
+
+
 
 // } Driver Code Ends
