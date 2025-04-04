e275feaad6930c0e1db8c77990806b43af687be6
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-04-04 19:21:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -4,42 +4,41 @@ using namespace std;
 
 
 // } Driver Code Ends
-
 class Solution {
   public:
-    bool dfs(int node, unordered_map<int, bool>& visited, unordered_map<int, list<int>>& adj, int parent) {
-    visited[node] = true;
-    for (auto &i : adj[node]) {
-        if (i == parent)
-            continue;
-        if (visited[i])
-            return true;
-        if (dfs(i, visited, adj, node))
-            return true;
-    }
-    return false;
-}
-
-bool isCycle(int V, vector<vector<int>>& edges) {
-    unordered_map<int, list<int>> adj;
-    for (int i = 0; i < edges.size(); i++) {
-        int u = edges[i][0];
-        int v = edges[i][1];
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    unordered_map<int, bool> visited;
-    for (int i = 0; i < edges.size(); i++) {
-        for (int j = 0; j < edges[i].size(); j++) {
-            if (!visited[edges[i][j]] && dfs(edges[i][j], visited, adj, -1))
-                return true;
+    bool dfs(int node, int parent , unordered_map<int,bool>&visited , 
+    unordered_map<int,list<int>>&adj)
+    {
+        visited[node] = 1;
+        for(auto &i : adj[node])
+        {
+            if(i==parent)
+                continue;
+            if(visited[i])
+                return 1;
+            if(dfs(i,node,visited,adj))
+                return 1;
         }
+        return 0;
     }
-    return false;
-}
-
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        unordered_map<int,list<int>>adj;
+        for(int i = 0 ; i < edges.size() ; i++)
+        {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        unordered_map<int,bool>visited;
+        for (int i = 0; i < V; i++) 
+        {
+            if(!visited[i] && dfs(i,-1,visited,adj))
+                    return 1;
+        }
+        return 0;
+    }
 };
-
 
 //{ Driver Code Starts.
 
