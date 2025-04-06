beb4955bc0d06326582a8a38e9cc7519a1e0d9a2
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-04-07 00:02:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.cpp
+++ Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.cpp
@@ -74,33 +74,19 @@ Node* buildTree(string str) {
 
 
 // } Driver Code Ends
-// User function template for C++
 
-/*
-class Node {
-public:
-    int data;
-    Node* left;
-    Node* right;
 
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-*/
 class Solution {
   public:
-    // Function to find the height of a binary tree.
     int height(Node* node) {
-        // code here
-        if(node==NULL)
+        if(node == NULL)
             return -1;
         int left = height(node->left);
         int right = height(node->right);
-        return max(left,right)+1;
+        return max(left,right) + 1;
     }
 };
+
 
 //{ Driver Code Starts.
 
