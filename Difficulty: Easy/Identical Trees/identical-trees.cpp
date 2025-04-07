7fd4855646e40e731be5c80cc00df78010ded868
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-04-08 00:48:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Identical Trees/identical-trees.cpp
+++ Difficulty: Easy/Identical Trees/identical-trees.cpp
@@ -20,9 +20,9 @@ class Solution {
   public:
     bool isIdentical(Node *r1, Node *r2) 
     {
-        if (r1 == NULL && r2 == NULL) 
+        if (!r1 && !r2) 
             return true; 
-        if (r1 == NULL || r2 == NULL) 
+        if (!r1|| !r2) 
             return false; 
         if (r1->data != r2->data) 
             return false;
