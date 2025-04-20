0b5b5b3b6ec16d24039ef3506bc7d471eb168d45
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-04-21 01:24:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.cpp
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -4,39 +4,21 @@ using namespace std;
 
 
 // } Driver Code Ends
-// User function template for C++
+
 class Solution {
   public:
-    bool bs(vector<int>& arr , int target)
+    int missingNum(vector<int>& arr) 
     {
-        int low = 0;
-        int high = arr.size()-1;
-        while(low <= high)
-        {
-            int mid = low + (high-low)/2;
-            if(arr[mid]==target)
-                return true;
-            else if(arr[mid]>target)
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        return false;
+        int n = arr.size() + 1;
+        long long total = 1LL * n * (n + 1) / 2;  // Prevent overflow
+        long long sum = 0;
+        for (int num : arr)
+            sum += num;
+        return total - sum;
     }
-    int missingNumber(vector<int>& arr) {
-        std::sort(arr.begin(),arr.end());
-        int n = arr.size() , ans;
-        for(int i = 1 ; i<=n+1 ; i++)
-        {
-            if(bs(arr,i)==false)
-            {
-                ans = i;
-                break;
-            }
-        }
-        return ans;
-    }
+
 };
+
 
 //{ Driver Code Starts.
 
@@ -57,7 +39,7 @@ int main() {
             a.push_back(num);
 
         Solution obj;
-        cout << obj.missingNumber(a) << endl;
+        cout << obj.missingNum(a) << endl;
         cout << "~\n";
     }
 
