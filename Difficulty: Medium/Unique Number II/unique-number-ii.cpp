257f1d4be30f9be1e546daa44f53c77aebb670bb
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-04-23 02:05:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Number II/unique-number-ii.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> singleNum(vector<int>& arr) {
+        unordered_map<int,int>freq;
+        for(int i : arr)
+            freq[i]++;
+        vector<int>ans;
+        for(int i : arr)
+        {
+            if(freq[i]==1)
+                ans.push_back(i);
+        }
+        if(ans[0]>ans[1])
+            swap(ans[0],ans[1]);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.singleNum(arr);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
