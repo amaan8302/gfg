c30c8d239b64aeadb0641fc4976e6d4924ea874f
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-06-19 11:44:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,17 +1,9 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User Function Template
 class Solution {
   public:
-    typedef pair<int, int> p;
-    vector<int> dijkstra(int V, vector<vector<int>> &edges, int S) {
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
         unordered_map<int,list<pair<int,int>>>adj;
-        for(int i = 0 ; i < edges.size() ; i++)
+        for(int i = 0 ; i < edges.size() ;i++)
         {
             int u = edges[i][0];
             int v = edges[i][1];
@@ -19,62 +11,25 @@ class Solution {
             adj[u].push_back({v,w});
             adj[v].push_back({u,w});
         }
-        vector<int> distance(V, INT_MAX);
-        set<p>setds;
-        distance[S] = 0;
-        setds.insert({0, S});
-        while (!setds.empty()) {
-            p top = *(setds.begin());
-            int nodeDistance = top.first;
-            int node = top.second;
-            setds.erase(setds.begin());
-            for (auto neighbor : adj[node]) {
-                int adjNode = neighbor.first;
-                int edgeWeight = neighbor.second;
-                if (nodeDistance + edgeWeight < distance[adjNode]) {
-                    auto it = setds.find(make_pair(distance[adjNode], adjNode));
-                    if (it != setds.end())
-                        setds.erase(it);
-                    distance[adjNode] = nodeDistance + edgeWeight;
-                    setds.insert(make_pair(distance[adjNode], adjNode));
+        vector<int>distance(V,INT_MAX);
+        distance[src]=0;
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>>pq;
+        pq.push({0,src});
+        while(!pq.empty()){
+            int d = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            for(auto &i : adj[node])
+            {
+                int nd = i.first;
+                int wt = i.second;
+                if(wt+d < distance[nd])
+                {
+                    distance[nd] = wt+d;
+                    pq.push({wt+d , nd});
                 }
             }
         }
         return distance;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> edges;
-        int i = 0;
-        while (i++ < E) {
-            int u, v, w;
-            cin >> u >> v >> w;
-            edges.push_back({u, v, w});
-            edges.push_back({v, u, w});
-        }
-        int src;
-        cin >> src;
-        cin.ignore();
-
-        Solution obj;
-        vector<int> res = obj.dijkstra(V, edges, src);
-
-        for (int i = 0; i < V; i++)
-            cout << res[i] << " ";
-        cout << endl;
-
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-// } Driver Code Ends
