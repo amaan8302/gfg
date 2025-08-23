0600d1467102a6983faebbb058bb8b23cd35e6fc
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-08-23 20:13:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,90 +1,38 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    bool isPossible(vector<int>& arr, int n, int m,int mid)
+    bool isPossible(vector<int>& arr, int k, int mid) 
     {
-        int student = 1;
-        int pages = 0;
-        for(int i : arr)
-        {
-            if(i>mid) return 0;
-            if(i + pages <= mid)
-                pages = pages + i;
-            else
-            {
-                student++;
-                if(student>m)
-                    return false;
-                pages = i;
+        int students = 1, pages = 0;
+        for (int pagesInBook : arr) {
+            if (pages + pagesInBook > mid) {
+                students++;
+                pages = pagesInBook;
+                if (students > k) return false;
+            } else {
+                pages += pagesInBook;
             }
         }
         return true;
     }
-    int findPages(vector<int> &arr, int m) {
-        // code here
+
+    int findPages(vector<int> &arr, int k) 
+    {
         int n = arr.size();
-        if(n<m)
-            return -1;
-        int sum=0;
-        for(int i : arr)
-            sum = sum + i;
-        int low = 0;
-        int high = sum;
-        int ans=-1;
-        while(low<=high)
-        {
-            int mid = low + (high-low)/2;
-            if(isPossible(arr,n,m,mid))
-            {
+        if (k > n) return -1;
+    
+        int low = *max_element(arr.begin(), arr.end());
+        int high = accumulate(arr.begin(), arr.end(), 0);
+        int ans = -1;
+    
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isPossible(arr, k, mid)) {
                 ans = mid;
                 high = mid - 1;
-            }
-            else
+            } else {
                 low = mid + 1;
+            }
         }
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int d;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        d = crr[0];
-        int n = arr.size();
-        Solution ob;
-        int ans = ob.findPages(arr, d);
-        cout << ans << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
