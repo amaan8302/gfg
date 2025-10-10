cd4ee64fb1bf39622819d0fd5132a5a770f39c49
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-10-11 00:06:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum path sum/maximum-path-sum.cpp
@@ -0,0 +1,30 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    int solve(Node* root, int &maxSum) {
+    if (root == NULL) return 0;
+    int left = max(0, solve(root->left, maxSum));
+    int right = max(0, solve(root->right, maxSum));
+    int currentPathSum = root->data + left + right;
+    maxSum = max(maxSum, currentPathSum);
+    return root->data + max(left, right);
+}
+
+int findMaxSum(Node* root) {
+    int maxSum = INT_MIN;
+    solve(root, maxSum);
+    return maxSum;
+}
+};
